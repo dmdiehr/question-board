91e4c407a542f0763cf6f427ec730107128fca53
dmdiehr <ddiehr@yahoo.com>
2016-04-02 12:48:57 -0700

    Adds a new-answer component.

---
+++ app/components/new-answer.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    newAnswerForm() {
+      this.set('addNewAnswer', true);
+      this.set('text', "");
+      this.set('author', "");
+    },
+
+    saveAnswer() {
+      var params = {
+        text: this.get('text') ? this.get('text') : "",
+        author: this.get('author') ? this.get('author') : "Anonymous",
+        question: this.get('question')
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
+});
